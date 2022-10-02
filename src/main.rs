d91809a23e54e5869ec674cae898ea66fa8e6dd0
LittleB0xes <nihenriot@gmail.com>
2022-10-02 23:29:45 +0200

    Create animated stuff... and good night

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::collections::HashMap;
+
 use macroquad::prelude::*;
+
+use sprite_library::*;
+mod sprite_library;
+
+use sprite::Sprite;
+mod sprite;
 
 struct Game {
     texture: Texture2D,
+    atlas: HashMap<String, SpriteLibraryData>,
+    scale: f32,
+
+    hero: Sprite,
 }
 
 impl Game {
@@ -9,15 +21,25 @@ impl Game {
         let texture = Texture2D::from_file_with_format(include_bytes!("../assets/spritesheet.png"), None);
         texture.set_filter(FilterMode::Nearest);
 
+        let atlas = read_atlas().unwrap();
+        let hero = Sprite::new(extract_data(&atlas, "hero_walk_right".to_string()));
+
         Self {
             texture,
+            atlas,
+            scale: 4.0,
+            hero,
 
         }
     }
 
-    fn update(&mut self) {}
+    fn update(&mut self) {
+    }
 
-    fn render(&mut self) {}
+    fn render(&mut self) {
+        self.hero.draw_sprite(self.texture, self.scale);
+        
+    }
 }
 
 
---
+++ src/sprite/mod.rs
@@ -0,0 +1,72 @@
+use macroquad::prelude::*;
+
+use crate::sprite_library::SpriteLibraryData;
+
+pub struct Sprite {
+    position: Vec2,
+    source_rect: Rect,
+    speed: i32,
+    frames: i32,
+    elapsed: i32,
+    current_frame: i32,
+}
+
+impl Sprite {
+    pub fn new(data: SpriteLibraryData) -> Self {
+        let source_rect = Rect::new(
+            data.x as f32,
+            data.y as f32,
+            data.w as f32,
+            data.h as f32);
+        
+        Self {
+            position: Vec2::ZERO,
+            source_rect,
+            frames: data.frame,
+            speed: data.speed,
+            elapsed: 0,
+            current_frame: 0,
+        }
+    }
+    
+    fn animate(&mut self) {
+        self.elapsed += 1;
+        if self.elapsed > self.speed {
+            self.current_frame = (self.current_frame + 1) % self.frames;
+            self.elapsed = 0;
+        }
+    }
+
+    pub fn draw_sprite(&mut self,texture: Texture2D, scale: f32) {
+        self.animate();
+        let current_source_rect = Rect {
+            x: self.source_rect.x + self.source_rect.w * self.current_frame as f32,
+            y: self.source_rect.y,
+            w: self.source_rect.w,
+            h: self.source_rect.h
+        };
+        let params = DrawTextureParams{
+            source: Some(current_source_rect),
+            dest_size: Some(Vec2::new(16.0 * scale, 16.0 * scale)),
+            rotation: 0.0,
+            flip_x: false,
+            flip_y: false,
+            pivot: None,
+
+        };
+
+        draw_texture_ex(texture, 10.0, 10.0, WHITE, params);
+
+    }
+    
+    pub fn current_frame(&self) -> Rect {
+       Rect {
+           x: self.source_rect.x + self.source_rect.w * self.current_frame as f32,
+           y: self.source_rect.y,
+           w: self.source_rect.w,
+           h: self.source_rect.h
+       }
+    }
+
+}
+
